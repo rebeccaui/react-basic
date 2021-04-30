c6baeb476403695504c29ba46c7a3c15036b769e
James Cowart <james@Jamess-Macbook.local>
2021-04-30 17:14:53 -0400

    ensures cart doesn't empty when other action is used

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon' />
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCount}</span>
     </div>
 );
 
@@ -18,7 +19,14 @@ const mapDisatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
+// selector takes slice of state
+// uses memoization: the linear caching of the selector's value to be used later
+const mapStateToProps = state => ({
+    // itemCount taken from cart.selectors.js
+    itemCount: selectCartItemsCount(state)
+});
+
 export default connect(
-    null,
+    mapStateToProps,
     mapDisatchToProps
 )(CartIcon);
