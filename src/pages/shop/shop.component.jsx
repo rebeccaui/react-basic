bbd1b44dfb7f9dbd2ce68e7e1b0e39c60410eb56
James Cowart <james@Jamess-Macbook.local>
2021-05-07 19:20:24 -0400

    added collection component and mapped collections to display on shop page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
 // import CollectionPreview from '../../components/collection-preview/collection-preview';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-const ShopPage = ({ collections }) => (
+const ShopPage = ({ match }) => (
     <div className='shop-page'>
-        <CollectionsOverview />
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        {/* this makes fetching the correct path simpler */}
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
 );
 
